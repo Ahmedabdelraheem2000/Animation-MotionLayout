83d394bd735aa760f378a87a69ec801c36678d8a
Simple Login and Register Activity <ahmedkinga60@gmail.com>
2022-12-06 02:36:08 +0200

    Initial commit

---
+++ app/src/main/java/com/halawy/animation/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.halawy.animation
+
+import android.graphics.Color
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import android.view.WindowManager.LayoutParams.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window?.decorView?.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+        window.statusBarColor = Color.TRANSPARENT
+
+        setContentView(R.layout.activity_main)
+
+    }
+}
